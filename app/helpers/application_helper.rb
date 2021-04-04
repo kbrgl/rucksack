1d196c8ebb6f901f6979d70a6067a37c5b7d65ff
Kabir Goel <kabirgoel.kg@gmail.com>
2021-04-04 13:08:53 +0530

    Fix bug in logo resolution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
   def format_date(date)
-    date.strftime("%b %d, %Y")
+    date.strftime('%b %d, %Y')
   end
 
   def logo_url
     if Newsletter.logo.attached?
       url_for(Newsletter.logo)
+    else
+      'logo.png'
     end
-    'logo.png'
   end
 end
